cbe917b17c6c0a0d8c41401ea9da41f628662a3f
Nischal <baniyanisal12@gmail.com>
2024-05-31 13:07:31 +0545

    changes in the db connection

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
 exports.databaseConnection = async function () {
-  return await mongoose.connect("mongodb://127.0.0.1:27017/filim_ticketing");
+  // return await mongoose.connect("mongodb://127.0.0.1:27017/filim_ticketing");
+  return await mongoose.connect("mongodb+srv://nischal:Cc8Eeu9qe.A!Xei@cluster0.gljarkj.mongodb.net/filim_ticketing")
 };
+
+
+//nischal
+// Cc8Eeu9qe.A!Xei
+// mongodb+srv://nischal:<password>@cluster0.gljarkj.mongodb.net/
